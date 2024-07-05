31c0947611170c3e5003217f97fbb7189c49b561
Mohit07122 <125998048+Mohit07122@users.noreply.github.com>
2024-07-05 22:03:56 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -21,7 +21,8 @@ class Var(object):
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001798208356'))
     NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1001798208356'))
     PORT = int(getenv('PORT', '8080'))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '95.216.97.247
+'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "5397984467").split()]
     NO_PORT = bool(getenv('NO_PORT', False))
